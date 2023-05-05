63e8cfd5bd3c04d7a58692a87edda6b20cfc6bc5
RedbeanGit <dubois.julien.mail@gmail.com>
2023-05-05 16:41:38 +0200

    fix: remove eureka
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

--- src/main/java/fr/dopolytech/polyshop/payment/PaymentApplication.java
+++ src/main/java/fr/dopolytech/polyshop/payment/PaymentApplication.java
@@ -2,10 +2,8 @@ package fr.dopolytech.polyshop.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class PaymentApplication {
 
 	public static void main(String[] args) {
